3f645b9e78b6c0b4452ee9dc07f4e5c19d612616
d_liyanage <admliyanage@gmail.com>
2019-09-23 13:31:47 +0530

    add register func

---
+++ src/components/Register.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <h1>Register</h1>
+        <input type="email" name="email" placeholder="E-mail here" v-model="email">
+        <br>
+        <input type="password" name="password" placeholder="Password here" v-model="password">
+        <br>
+        <button @click="register">Register</button>
+    </div>
+</template>
+
+<script>
+/*eslint-disable*/
+import AuthenticationService from '@/services/AuthenticationService'
+
+export default {
+    data() {
+        return {
+            email:'',
+            password:''
+        }
+    },
+    methods: {
+       async register(){
+            const response = await AuthenticationService.register({
+                   email: this.email,
+                   password:this.password
+            }) 
+            console.log(response.data);
+            
+        }
+    },
+    // watch: {
+    //     email(value){
+    //         console.log('email has changed to ',value);            
+    //     }
+    // },
+    // mounted() {
+    //     setTimeout(() => {
+    //         this.email = 'hello world'
+    //     }, 3000);
+    // },
+}
+</script>
+
+<style>
+
+</style>
